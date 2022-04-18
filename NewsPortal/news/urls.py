49ac414f5fdfdcff54c21d506782c8759b0d699a
evgenei0904 <98351234+evgenei0904@users.noreply.github.com>
2022-04-18 19:41:12 +0300

    Add files via upload

---
+++ NewsPortal/news/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from .views import NewsList, NewsDetail, NewsSearch, NewsCreate, NewsUpdate, NewsDelete
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', NewsList.as_view(), name='news_list'),
+    path('<int:pk>', NewsDetail.as_view(), name='news_detail'),
+    path('search', NewsSearch.as_view()),
+    path('add', NewsCreate.as_view(), name='news_create'),
+    path('<int:pk>/edit/', NewsUpdate.as_view(), name='news_update'),
+    path('<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+
+]
+
+
